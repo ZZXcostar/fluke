c4dd10328d736902d55800910c914b1838172459
陈国强 <293599505@qq.com>
2021-07-04 22:25:03 +0800

    dev: 修改

--- index.js
+++ index.js
@@ -36,14 +36,71 @@ function cnOver() {
     },200);
     
 }
-function overImg(index) {
-    console.log()
-    $(".img2box img").eq(index).hide()
-}
 $(".img1").fadeOut({duration: 1000})
 $(".img2").fadeIn({duration: 1000})
 })
 function cnver2() {
     $(".an2Title .cn").fadeOut()
+    $(".img2box").fadeOut()
+    setTimeout(function(){
+        $(".an3Title").show()
+        $(".an3Title").animate({
+            top:"100%",
+            right:"50%",
+            fontSize: '38px'
+        },2000);
+      
+      $(".an3Title .cn").animate({
+          fontSize: '12px'
+      },250,cnver3);
+      $(".img3box div").eq(0).animate({
+        left:"40%",
+        fontSize: '38px'
+    },1500,'swing')
+    $(".img3box div").eq(1).animate({
+        left:"30%",
+        top:"-50%",
+        fontSize: '38px'
+    },1350,'swing')
+    $(".img3box div").eq(2).animate({
+        left:"60%",
+        top:"-20%",
+        fontSize: '38px'
+    },1300,'swing')
+    $(".img3box div").eq(3).animate({
+        left:"90%",
+        top:"-10%",
+        fontSize: '38px'
+    },500,'swing')
+    $(".img3box div").eq(4).animate({
+        left:"80%",
+        top:"10%",
+        fontSize: '38px'
+    },1900,'swing')
+    $(".img3box div").eq(5).animate({
+        left:"30%",
+        top:"50%",
+        fontSize: '38px'
+    },2000,'swing')
+    $(".img3box div").eq(6).animate({
+        left:"20%",
+        top:"40%",
+        fontSize: '38px'
+    },1200,'swing')
+    $(".img3box div").eq(7).animate({
+        left:"40%",
+        top:"10%",
+        fontSize: '38px'
+    },3500,'swing')
+    $(".img3box div").eq(8).animate({
+        left:"0%",
+        top:"0%",
+        fontSize: '38px'
+    },4000,'swing')
+    },200);
+}
+function cnver3() {
+    $(".an3Title .cn").fadeOut()
     // $(".bg1").fadeOut()
+    
 }
