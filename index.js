b8d837895113b852a0f2b70a8d68328a260c9ca5
陈国强 <293599505@qq.com>
2021-07-07 01:52:10 +0800

    dev: xiugai

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(function(){
  $(".an1Title").animate({
-      left:"-=30%",
-      top:"-=50%",
-      fontSize: '38px'
+      left:"12%",
+      top:"-140%",
+      fontSize: '.2rem'
   },2000,'swing',cnOver);
 
 $(".an1Title .cn").animate({
@@ -25,9 +25,9 @@ function cnOver() {
         $(".img2box img").eq(9).fadeIn({duration: 4500});
         $(".an2Title").show()
         $(".an2Title").animate({
-            right:"-=60%",
-            top:"-=15%",
-            fontSize: '38px'
+            right:"-=42%",
+            top:"-=39%",
+            fontSize: '.15rem'
         },2000);
       
       $(".an2Title .cn").animate({
@@ -45,9 +45,9 @@ function cnver2() {
     setTimeout(function(){
         $(".an3Title").show()
         $(".an3Title").animate({
-            top:"100%",
-            right:"50%",
-            fontSize: '38px'
+            top:"150%",
+            right:"52%",
+            fontSize: '.16rem'
         },2000);
       
       $(".an3Title .cn").animate({
@@ -101,6 +101,8 @@ function cnver2() {
 }
 function cnver3() {
     $(".an3Title .cn").fadeOut()
+    $(".img3box").fadeOut()
+    goamian()
     // $(".bg1").fadeOut()
     
 }
@@ -109,19 +111,115 @@ $(".meritBox li").hover(function(){
     $(this).find(".imgText").show();
 });
 $(".meritBox li").mouseleave(function(){
-    $(".meritBox .imgText").hide();
+    // $(".meritBox .imgText").hide();
 });
 
+function goamian(){
+    $(".lineRight").fadeIn()
+    $(".lineImg3").addClass("intro");
+    $('.lineImg3').animate({left:'83%',opacity: '0'},2000,setblack1);
+    $('.lineImgLine3').animate({left:'0%',width: '100%'},2000);
+    setTimeout(() => {
+      $(".lineTop").fadeIn()
+      $(".lineImg1").addClass("intro");
+      $('.lineImg1').animate({left:'83%',opacity: '0'},2000,setblack2);
+      $('.lineImgLine1').animate({left:'0%',width: '100%'},2000);
+      $('.lineImg1').css('left', '0%')
+    }, 2000);
+    setTimeout(() => {
+      $(".lineLeft").fadeIn()
+      $(".lineImg2").addClass("intro");
+      $('.lineImg2').animate({left:'83%',opacity: '0'},2000,setblack3);
+      $('.lineImgLine2').animate({left:'0%', width: '100%'},2000);
+      $('.lineImg2').css('left', '0%')
+    }, 4000);
+
+    function setblack1(){
+      $('.lineImg3').css('left', '0%')
+      $('.lineImg3').removeClass('intro')
+    }
+    function setblack2(){
+      $('.lineImg1').css('left', '0%')
+      $('.lineImg1').removeClass('intro')
+    }
+    function setblack3(){
+      $('.lineImg2').css('left', '0%')
+      $('.lineImg2').removeClass('intro')
+    }
+    $(".product1_center").fadeIn(2000)
+    setTimeout(() => {
+        let css = {left:'83%'};
+        let interval = null
+        clearInterval(interval)
+        interval = setInterval(function(){ // 这里要改成回调 定时器有性能问题
+          $(".lineImg1").addClass("intro");
+          $(".lineImg2").addClass("intro");
+          $(".lineImg3").addClass("intro");
+
+          $('.lineImg1').animate(css,2000,rowBack1);
+          $('.lineImg2').animate(css,2000,rowBack2);
+          $('.lineImg3').animate(css,2000,rowBack3);
+        },0);
+        function rowBack1(){
+            if(css.left==='83%'){
+                $('.lineImg1').css('left', '0%')
+                $(".lineImg1").removeClass("intro");
+                // $('.lineImg1').fadeOut()
+            }
+        }
+        function rowBack2(){
+            if(css.left==='83%'){
+                $('.lineImg2').css('left', '0%')
+                $(".lineImg2").removeClass("intro");
+                // $('.lineImg2').fadeOut()
+            }
+        }
+        function rowBack3(){
+            if(css.left==='83%'){
+                $('.lineImg3').css('left', '0%')
+                $(".lineImg3").removeClass("intro");
+                // $('.lineImg3').fadeOut()
+            }
+        }
+    }, 6500);
+    
+}
+
 function ShowDiv(show_div, bg_div) {
-    document.getElementById(show_div).style.display = 'block';
-    document.getElementById(bg_div).style.display = 'block';
+    $("#"+show_div).fadeIn(1000)
+    $("#"+bg_div).fadeIn(1000)
     var bgdiv = document.getElementById(bg_div);
     bgdiv.style.width = document.body.scrollWidth;
-    // bgdiv.style.height = $(document).height();
     $("#" + bg_div).height($(document).height());
    };
    //关闭弹出层
    function CloseDiv(show_div, bg_div) {
-    document.getElementById(show_div).style.display = 'none';
-    document.getElementById(bg_div).style.display = 'none';
+    $("#"+show_div).fadeOut(1000)
+    $("#"+bg_div).fadeOut(1000)
    };
+
+  function isCellphone(str) {
+    /**
+    *@descrition:手机号码段规则
+    * 13段：130、131、132、133、134、135、136、137、138、139
+    * 14段：145、147
+    * 15段：150、151、152、153、155、156、157、158、159
+    * 17段：170、176、177、178
+    * 18段：180、181、182、183、184、185、186、187、188、189
+    * 
+    */
+    var pattern =  /^(13[0-9]|14[57]|15[012356789]|17[0678]|18[0-9])\d{8}$/;
+    return pattern.test(str);
+}
+function isEmail(str){
+    /**
+    * @descrition:邮箱规则
+    * 1.邮箱以a-z、A-Z、0-9开头，最小长度为1.
+    * 2.如果左侧部分包含-、_、.则这些特殊符号的前面必须包一位数字或字母。
+    * 3.@符号是必填项
+    * 4.右则部分可分为两部分，第一部分为邮件提供商域名地址，第二部分为域名后缀，现已知的最短为2位。最长的为6为。
+    * 5.邮件提供商域可以包含特殊字符-、_、.
+    */
+    var pattern = /^([a-zA-Z0-9]+[-_.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[-_.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,6}$/;
+    return pattern.test(str);
+}
